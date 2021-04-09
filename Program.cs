cd46b98cdada2d2d1978ac423ed07a4a1493539a
Filip Nilsson <filmarnil@gmail.com>
2021-04-09 20:35:05 +0200

    Use Write instead of WriteLine to get input on same row

--- Program.cs
+++ Program.cs
@@ -11,14 +11,16 @@ namespace minesweeper
 
             while (game.State == GameState.Running)
             {
-                Console.WriteLine("Reveal position: ");
+                Console.Write("Reveal position: ");
                 var input = System.Console.ReadLine();
+
                 var match = Regex.Match(input, @"(\d+) (\d+)");
                 if (match.Success)
                 {
                     var row = int.Parse(match.Groups[1].Value);
                     var column = int.Parse(match.Groups[2].Value);
                     var position = new Position(row, column);
+
                     game.HandleInput(Action.Reveal, position);
                     game.PrintBoard();
                 }
