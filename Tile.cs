421984256f9793522c1865d929b1b50ca9d9d647
Filip Nilsson <filmarnil@gmail.com>
2021-04-09 19:55:36 +0200

    Add property used to see if tile has been cleared

--- Tile.cs
+++ Tile.cs
@@ -6,5 +6,9 @@ namespace minesweeper
         public TileState State { get; init; }
         public bool HasMine { get; init; }
         public int AdjacentMines { get; init; }
+        public bool Cleared
+        {
+            get => State == TileState.Revealed || State == TileState.Exploded;
+        }
     }
 }
