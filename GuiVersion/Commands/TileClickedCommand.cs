6be886151ab8cf706f08a8719788be72b9b7cb1c
filipni <filip.nilsson+github@protonmail.com>
2021-04-14 22:38:09 +0200

    Add command

---
+++ GuiVersion/Commands/TileClickedCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using GuiVersion.ViewModels;
+using SweeperCore;
+
+namespace GuiVersion.Commands
+{
+    public class TileClickedCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly ViewModel _viewModel;
+
+        public TileClickedCommand(ViewModel vm) => _viewModel = vm;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            if (parameter is Position position)
+            {
+                _viewModel.HandleTileClicked(position);
+            }
+        }
+    }
+}
