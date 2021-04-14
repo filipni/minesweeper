6d6885f9beb3f591080fc2140cdc903ecee83743
filipni <filip.nilsson+github@protonmail.com>
2021-04-14 22:38:24 +0200

    Add converters

---
+++ GuiVersion/Converters/FilePathToImageSourceConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using SweeperCore;
+
+namespace GuiVersion.Converters
+{
+    [ValueConversion(typeof(TileImage), typeof(SolidColorBrush))]
+    class TileImageToBackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TileImage image)
+            {
+                return image switch
+                {
+                    TileImage.Hidden => new SolidColorBrush(Colors.LightBlue),
+                    TileImage.Exploded => new SolidColorBrush(Colors.OrangeRed),
+                    _ => new SolidColorBrush(Colors.White)
+
+                };
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ GuiVersion/Converters/TileImageToBackgroundColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace GuiVersion.Converters
+{
+    [ValueConversion(typeof(string), typeof(ImageSource))]
+    class FilePathToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string path)
+            {
+                var converter = new ImageSourceConverter();
+                var imageSource = converter.ConvertFromString(path) as ImageSource;
+                return imageSource;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
