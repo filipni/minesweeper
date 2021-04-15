49e71f5e66d02bfdb9a678974425f7550f69c3bc
filipni <filip.nilsson+github@protonmail.com>
2021-04-15 15:59:21 +0200

    Update interface to only contain setters

--- SweeperCore/IView.cs
+++ SweeperCore/IView.cs
@@ -3,8 +3,8 @@ namespace SweeperCore
     public interface IView
     {
         GameState State { set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
+        public int Width { set; }
+        public int Height { set; }
 
         void ResetBoard();
         void UpdateTile(Position position, TileImage image);
