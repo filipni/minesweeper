ff535f5d73d617b65d657d4aa78f7753ba938eb3
filipni <filip.nilsson+github@protonmail.com>
2021-05-14 23:35:00 +0200

    Simplify property getters

--- SweeperCore/Tile.cs
+++ SweeperCore/Tile.cs
@@ -5,30 +5,23 @@ namespace SweeperCore
     record Tile(Position Position, bool HasMine, int AdjacentMines, TileState State = TileState.Hidden)
     {
         public bool Cleared
-        {
-            get => State == TileState.Revealed || State == TileState.Exploded;
-        }
+            => State == TileState.Revealed || State == TileState.Exploded;
 
         public bool Marked
-        {
-            get => State == TileState.Flagged || State == TileState.Questioned;
-        }
+            => State == TileState.Flagged || State == TileState.Questioned;
 
         public TileImage Image
-        {
-            get =>
-                State switch
-                {
-                    TileState.Hidden => TileImage.Hidden,
-                    TileState.Exploded => TileImage.Exploded,
-                    TileState.Flagged => TileImage.Flagged,
-                    TileState.Questioned => TileImage.Questioned,
-                    TileState.Revealed when HasMine => TileImage.Mine,
-                    TileState.Revealed when AdjacentMines != 0 => _adjacentMinesImages[AdjacentMines],
-                    TileState.Revealed when AdjacentMines == 0 => TileImage.Cleared,
-                    _ => TileImage.Cleared
-                };
-        }
+            => State switch
+            {
+                TileState.Hidden => TileImage.Hidden,
+                TileState.Exploded => TileImage.Exploded,
+                TileState.Flagged => TileImage.Flagged,
+                TileState.Questioned => TileImage.Questioned,
+                TileState.Revealed when HasMine => TileImage.Mine,
+                TileState.Revealed when AdjacentMines != 0 => _adjacentMinesImages[AdjacentMines],
+                TileState.Revealed when AdjacentMines == 0 => TileImage.Cleared,
+                _ => TileImage.Cleared
+            };
 
         private static readonly Dictionary<int, TileImage> _adjacentMinesImages = new()
         {
